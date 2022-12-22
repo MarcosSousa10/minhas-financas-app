26537f1ceed81afd319a2523aedfc60e32112c53
marcos <marcospegodesousa10@gmail.com>
2022-12-22 19:21:11 -0300

    2212

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootswatch/dist/flatly/bootstrap.css'
 import React from 'react';
 import Login from './views/login';
+import './custom.css';
 class App extends React.Component {
   render(){
     return (
